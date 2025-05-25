01cc9a25041e227c3f262aa4c341161e9c94ff79
aliakborswe <aliakbor.js@gmail.com>
2025-05-25 11:24:51 +0600

    rangers table created

--- postgreSQL_assignment.sql
+++ postgreSQL_assignment.sql
@@ -1,2 +1,8 @@
 -- Active: 1747652023164@@localhost@5432@conservation_db
 CREATE DATABASE conservation_db;
+
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    region VARCHAR(100) NOT NULL
+);
